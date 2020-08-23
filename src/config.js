03b0734627619698a9b30c09fa997be550789d17
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-22 22:04:38 -0700

    deployment changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DB_URL:process.env.TEST_DB_URL
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:new123@localhost/noteful',
+  TEST_DATABASEB_URL:process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:new123@localhost/noteful_test'
 };
